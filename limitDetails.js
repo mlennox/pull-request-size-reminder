1e32872c1c5c51cc92fb03e5e07dd5eac8a86002
Mark Lennox <mark@webpusher.ie>
2019-05-20 23:16:18 +0100

    using bright and bold colours now

--- limitDetails.js
+++ limitDetails.js
@@ -8,23 +8,23 @@ module.exports = [
     maxNumber: 15,
     title: 'Great!',
     message: 'Good job! your pull request will be easy to review',
-    colour: chalk.black.bgGreen.bold,
+    colour: chalk.whiteBright.bgGreen.bold,
   },
   {
     maxNumber: 25,
     title: 'Careful!',
     message: 'Your pull request is getting a little bit on the large side, keep an eye on it',
-    colour: chalk.red.bgYellow,
+    colour: chalk.redBright.bgYellowBright,
   },
   {
     maxNumber: 40,
-    title: 'A big PR!',
-    message: 'Woah! Your pull request is large, you may have your work cut out convincing folks to review it!',
-    colour: chalk.white.bgRed.bold
+    title: 'Woah! A big PR!',
+    message: 'Your pull request is large, think of your colleagues!',
+    colour: chalk.whiteBright.bgRedBright.bold
   },
   {
     title: 'Very large PR!',
     message: 'The number of files is too damn high!',
-    colour: chalk.blue.bgMagenta.bold
+    colour: chalk.yellowBright.bgMagenta.bold
   }
 ]
